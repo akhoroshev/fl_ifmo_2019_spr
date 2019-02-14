507be38ba2f7bca2971d0f8bd239fe248ef8b06b
akhoroshev <arthoroshev@gmail.com>
2019-02-15 02:19:21 +0300

    Implement Own Parser with "fast" KeyWord parsing based on Trie

--- Main.hs
+++ Main.hs
@@ -2,13 +2,17 @@ module Main where
 
 import Tokenizer
 
-runTokenizer :: String -> IO ()
+runTokenizer :: String -> IO [Token]
 runTokenizer input = do
-  putStrLn input
-  putStrLn $ show $ tokenize input
-  putStrLn ""
+  putStrLn $ "Input: " ++ input
+  let result = tokenize input
+  putStrLn $ "Output: " ++ (show result)
+  return result
 
 main :: IO ()
 main = do
-  runTokenizer " 1 2 abc if "
-  runTokenizer " "
+    [Number 1, Number 2, Ident "abc", KeyWord "if"] <- runTokenizer " 1 2 abc if "
+    [Number 1, Number 2, Ident "abcif", KeyWord "if"] <- runTokenizer " 1 2 abcif if "
+    [Number 1, Number 2, KeyWord "for", KeyWord "if"] <- runTokenizer " 1 2 for if "
+    [Number 0, Number 12] <- runTokenizer " 0 12"
+    return ()
--- Tokenizer.hs
+++ Tokenizer.hs
@@ -1,6 +1,7 @@
 module Tokenizer where
 
-import Combinators
+import Combinators (Parser, keywords, char, string, some, many, runParser, digit, letter, choice, eof)
+import Control.Applicative ((<|>))
 
 data Token = Ident String
            | KeyWord String
@@ -8,13 +9,36 @@ data Token = Ident String
            deriving (Show, Eq)
 
 tokenize :: String -> [Token]
-tokenize input = undefined
+tokenize input = case runParser (many doAct <* eof) input of
+                      Just (_, tokens) -> tokens
+                      _ -> []
 
 parseIdent :: Parser String String
-parseIdent = undefined
+parseIdent = do
+    let allowsymbol = letter <|> char '_'
+    x <- allowsymbol
+    xs <- many (allowsymbol <|> digit)
+    return (x:xs)
 
 parseKeyWord :: Parser String String
-parseKeyWord = undefined
+parseKeyWord = keywords ["and", "del", "global", "not", "withas", "elif", "if", "or", "yieldassert", "else", "import", "pass", "Falsebreak", "except", "in", "raise", "Noneclass", "finally", "is", "return", "Truecontinue", "for", "lambda", "trydef", "from", "nonlocal", "while"]
 
 parseNumber :: Parser String Int
-parseNumber = undefined
+parseNumber = fmap (read) $
+    string "0" <|>
+    do 
+        let nonzerodigit = choice (char <$> "123456789")
+        x <- nonzerodigit
+        xs <- many digit
+        return (x:xs)
+        
+(<||>) :: Parser s a -> Parser s b -> Parser s ()
+(<||>) a b = (const () <$> a) <|> (const () <$> b)
+    
+doAct :: Parser String Token
+doAct = (skip *> (KeyWord <$> parseKeyWord) <* skip1) <|> 
+        (skip *> (Number <$> parseNumber)   <* skip1) <|>
+        (skip *> (Ident <$> parseIdent)     <* skip1)
+            where
+                skip = many $ char ' '
+                skip1 = (some $ char ' ') <||> eof
